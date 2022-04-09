bdcac469aca927a63cbf195c18630f3294d29235
Keks <keks@htmlacademy.ru>
2022-04-09 12:48:34 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/main.js
@@ -0,0 +1,7 @@
+import './data.js';
+import './preview.js';
+import './big-picture.js';
+import './user-form.js';
+// import {createUserPictures} from './data.js';
+
+// console.log(createUserPictures());
